0d462efe03800d92b1d3c8b2a87fee24ec3d242a
Nathalia Acosta Vélez <nathalia285@gmail.com>
2024-05-15 11:28:44 -0500

    rutas y versión 2 de componentes iniciales

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import './styles.css'
+
+const Home = () => {
+  return (
+    <main className='home3'>
+      <h1 className='text-3xl font-bold underline'>Home</h1>
+      <p>Some contents...</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla aliquid excepturi reprehenderit hic amet? Nisi aliquid error deserunt illum ducimus commodi iusto iure id harum alias aut, assumenda deleniti in!</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor modi error, nulla fugiat nam necessitatibus, sit praesentium doloribus ratione maxime similique debitis iste a ipsum hic quae perferendis, non reiciendis?</p>
+    </main>
+  )
+}
+
+export default Home
