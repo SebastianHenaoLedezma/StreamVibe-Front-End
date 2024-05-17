6423c748f26aeabb0e9f9b2a79dc7b578e17183e
Nathalia Acosta Vélez <nathalia285@gmail.com>
2024-05-17 10:39:59 -0500

    maquetado de componentes del Home

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './router/AppRoutes.jsx'
 import Home from './pages/Home/index.jsx'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <BrowserRouter>
