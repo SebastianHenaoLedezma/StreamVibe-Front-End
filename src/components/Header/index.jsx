799e3605781b5e80e72849a0657adb8da760e267
Jorge Arley Ospina <joarospina@utp.edu.co>
2024-05-19 21:23:19 -0500

    img movie play button

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -16,9 +16,9 @@ const Header = () => {
             <svg
               fill='none'
               stroke='currentColor'
-              stroke-linecap='round'
-              stroke-linejoin='round'
-              stroke-width='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
               viewBox='0 0 24 24'
               className='w-6 h-6'
             >
